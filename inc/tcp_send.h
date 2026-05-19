051ec137d1600c7f34c5cec895e4fbeab5883919
Guranjun <13868382860@163.com>
2026-05-19 22:17:03 +0800

    add:新增tcp上传数据，经测验可成功识别并显示tcp上传的图像数据

--- inc/tcp_send.h
+++ inc/tcp_send.h
@@ -1,6 +1,18 @@
 #ifndef __TCP_SEND_H
 #define __TCP_SEND_H
 
+#include "common.h"
 
-void* tcp_send_thread(void* arg);
-#endif //__TCP_SEND_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void *tcp_send_thread(void *arg);
+void tcp_msg_handler(Common_Msg_t *msg);
+void tcp_thread_wakeup(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __TCP_SEND_H
--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include "common.h"
 #include "v4l2_dev.h"
 #include "udp_send.h"
+#include "tcp_send.h"
 #include "msg_deliver.h"
 #include "alarm.hpp"
 #include "storage_video.hpp"
@@ -21,6 +22,7 @@ void stop_handler(int sig) {
     running = 0;
     msg_thread_wakeup(); // 关键：拍醒所有正在 cond_wait 的线程，让它们检查 running 标志并退出
     udp_thread_wakeup(); // 关键：拍醒所有正在 cond_wait 的线程，让它们检查 running 标志并退出
+    tcp_thread_wakeup();
     alarm_thread_wakeup();
     storage_thread_wakeup();
     logger_thread_wakeup();
@@ -40,15 +42,17 @@ int main(int argc,char **argv)
     signal(SIGINT, stop_handler);
     /*初始化共享缓冲区*/
     /*线程相关的定义*/
-    pthread_t t_camera_capture, t_udp_send, t_image_process, t_msg_deliver, t_image_storage, t_log_process;
+    pthread_t t_camera_capture, t_udp_send, t_tcp_send, t_image_process, t_msg_deliver, t_image_storage, t_log_process;
     pthread_create(&t_camera_capture, NULL, camera_capture_thread, (void *)argv[1]);
     pthread_create(&t_udp_send, NULL, udp_send_thread, (void *)argv[2]);
+    pthread_create(&t_tcp_send, NULL, tcp_send_thread, (void *)argv[2]);
     pthread_create(&t_image_process, NULL, process_image_thread, NULL);
     pthread_create(&t_msg_deliver, NULL, msg_deliver_thread, NULL);
     pthread_create(&t_image_storage, NULL, storage_video_thread, NULL);
     pthread_create(&t_log_process, NULL, logger_process_thread, NULL);
     pthread_join(t_camera_capture, NULL);
     pthread_join(t_udp_send, NULL);
+    pthread_join(t_tcp_send, NULL);
     pthread_join(t_image_process, NULL);
     pthread_join(t_msg_deliver, NULL);
     pthread_join(t_image_storage, NULL);
